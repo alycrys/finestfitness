d95799961a98e3e220b6a3114e9ef2fe9c562482
Alina Visan <alina.visan@centrica.com>
2019-06-18 15:44:00 +0100

    add some pages

---
+++ src/pages/About/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="page white">
+      <h3>About me</h3>
+      <p>
+        As a teen, I have competed in trampoline and "Savate" Boxing
+        Championships to a national level. This has definitely given me the
+        ability, the focus and the drive to achieve my goals.
+      </p>
+
+      <p>
+        I'm now sharing these experiences with you as a personal trainer to work
+        on yours!
+      </p>
+
+      <h3>Qualifications & Experience</h3>
+      <ul>
+        <li>Qualified Level 3 Personal Trainer</li>
+        <li>Registered with REPs (Register of Exercise Professionals)</li>
+        <li>Award in Strength and Conditioning</li>
+        <li>Award in Kettlebells</li>
+        <li>Award in Functional Exercises</li>
+        <li>Body Posture through Myofascial Release techniques practitioner</li>
+        <li>Experience with people suffering from obesity and diabetes</li>
+        <li>Trained First Aider</li>
+      </ul>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/pages/Training/Training.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Training() {
+  return (
+    <section className="page blue">
+      <h3>What can we achieve together?</h3>
+      <p>
+        Strength and Conditioning - Help you get stronger, faster, more flexible
+        and build your muscular endurance mixing plyometrics, speed and agility,
+        endurance, core stability and strength training.
+      </p>
+
+      <p>
+        Kettle Bell Training - Training in the three planes of motion, tapping
+        in to big power house muscles and smaller stability muscles at the same
+        time to achieve great results and better body control/awareness.
+      </p>
+
+      <p>
+        Functional Exercises & Body Posture - Become a new you! This could help
+        you to be joint pain free (lower back, neck, shoulders, knees), improve
+        your breathing, your digestion, your blood circulation, aesthetics,
+        non-verbal communication, flexibility, athletic performance, energy and
+        stable fat level!
+      </p>
+
+      <p>
+        Now imagine the new you if we mix those three styles of training!
+        Envision it and let's achieve it.
+      </p>
+    </section>
+  );
+}
+
+export default Training;
